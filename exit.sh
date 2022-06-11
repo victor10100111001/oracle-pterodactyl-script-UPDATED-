d24f08a205918fb66d311698d07d201d5bba8e02
JmantZZ <97107442+JmantZZ@users.noreply.github.com>
2022-06-11 18:23:50 +0300

    Add files via upload

---
+++ exit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#colors
+YELLOW='\033[1;33m'
+NC='\033[0m' 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+LRED='\033[1;31m'
+
+
+echo -e "${GREEN}"
+figlet -f slant "Thank's for using my script!"
+echo -e "${NC}"
+
+echo -e "${GREEN}INSTALLATION FINISHED!${NC}"
